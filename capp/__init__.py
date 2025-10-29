ad6b60e0b7253f7739134bbaaa92b0b17a7ab3cb
Sigurd Linde <Sigurdlinde@live.no>
2025-10-29 14:15:05 +0100

    Register, login og sqlite local database konfigurert

--- capp/__init__.py
+++ capp/__init__.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 import os
+from flask_login import LoginManager
 
 application = Flask(__name__)
 
 # application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
-
 application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+application.config['SQLALCHEMY_BINDS'] ={'transport': 'sqlite:///transport.db'}
+
+# Initialize extensions
+db = SQLAlchemy(application)
+bcrypt = Bcrypt(application)
+login_manager = LoginManager(application)
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+
+from capp import models
 
 from capp.home.routes import home
 from capp.methodology.routes import methodology
