893e624ec6dd1f871ef90aab508e062e5ee3b174
Ivanerbest <ivan.nenadovic1103@gmail.com>
2025-11-05 11:02:28 +0100

    endrigner ivan v5

--- capp/carbon_app/routes.py
+++ capp/carbon_app/routes.py
@@ -1,8 +1,15 @@
-from flask import render_template, Blueprint
-from flask_login import login_required
+from flask import render_template, Blueprint, request, redirect, url_for, flash
+from flask_login import login_required, current_user
 from capp.models import Transport
+from capp import db
+from datetime import timedelta, datetime
+from capp.carbon_app.forms import BusForm
 
 carbon_app=Blueprint('carbon_app',__name__)
+
+
+efco2={'Bus':{'Diesel':0.10,'CNG':0.08, 'Petrol':0.15,'No Fossil Fuel':0}}
+efch4={'Bus':{'Diesel':0.20,'CNG':0.08, 'Petrol':0.30,'No Fossil Fuel':0}}
 
 
 @carbon_app.route('/carbon_app')
@@ -10,12 +17,40 @@ carbon_app=Blueprint('carbon_app',__name__)
 def carbon_app_home():
     return render_template('carbon_app/carbon_app.html', title='carbon_app')
 
-@carbon_app.route('/carbon_app/new_entry')
+@carbon_app.route('/carbon_app/new_entry_bus', methods=['GET','POST'])
 @login_required
 def new_entry():
-    return render_template('carbon_app/new_entry.html', title='new_entry')
+    form =BusForm()
+    if form.validate_on_submit():
+        kms=form.kms.data
+        fuel=form.fuel_type.data
+        transport='Bus'
+        co2=float(kms)*efco2[transport][fuel]
+        ch4=float(kms)*efch4[transport][fuel]
+        total=co2+ch4
+        co2=float("{:.2f}".format(co2))
+        ch4=float("{:.2f}".format(ch4))
+        total=float("{:.2f}".format(total))
+        emissions=Transport(kms=kms, transport=transport, fuel=fuel,
+                            co2=co2, ch4=ch4, total=total, author=current_user)
+        db.session.add(emissions)
+        db.session.commit()
+        return redirect(url_for('carbon_app.your_data'))
+    return render_template('carbon_app/new_entry_bus.html', title='new_entry_bus', form=form)
 
 @carbon_app.route('/carbon_app/your_data')
+@login_required
 def your_data():
-    return render_template('carbon_app/your_data.html', title='your_data')
+    entries = Transport.query.filter_by(author=current_user).\
+        filter(Transport.date>(datetime.now()-timedelta(days=5))).\
+        order_by(Transport.date.desc()).order_by(Transport.transport.asc()).all()
+    return render_template('carbon_app/your_data.html', title='your_data', entries=entries)
+@carbon_app.route('/carbon_app/delete_emissions/<int:entry_id>')
+def delete_emission(entry_id):
+    entry=Transport.query.get_or_404(int(entry_id))
+    db.session.delete(entry)
+    db.session.commit()
+    flash("Entry deleted","sucsess")
+    return redirect(url_for('carbon_app.your_data'))
+
     
