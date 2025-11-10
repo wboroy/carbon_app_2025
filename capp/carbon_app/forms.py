e184066702645c9b33a8ad782c0d09d0c40ab86d
Ivanerbest <ivan.nenadovic1103@gmail.com>
2025-11-10 14:01:16 +0100

    calculator

--- capp/carbon_app/forms.py
+++ capp/carbon_app/forms.py
@@ -2,8 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, SelectField, FloatField
 from wtforms.validators import InputRequired
 
-class BusForm(FlaskForm):
+class TransportForm(FlaskForm):
+    transport = SelectField(
+        'Transport Type',
+        [InputRequired()],
+        choices=[
+            ('Bus', 'Bus'), ('Car', 'Car')])
     kms=FloatField('Kilometers', [InputRequired()])
     fuel_type= SelectField('Type of fuel', [InputRequired()],
         choices=[('Diesel', 'Diesel'),('Petrol', 'Petrol')])
-    submit=SubmitField('Submit') 
+    submit=SubmitField('Submit')    
+
